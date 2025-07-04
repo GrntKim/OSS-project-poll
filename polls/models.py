06b606b46eb4ee3ccf0e8f1c655bc151bc81d424
GrntKim <grantjun0224@gmail.com>
2025-07-04 10:27:01 +0900

    feat: Create Project model, Implement admin page and project editing feature

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+    project_author = models.CharField(max_length=200)
+    project_detail = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.project_name
+
+class Rating(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.score
